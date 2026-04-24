11ed35b48f848a0ac5186a3887b3371e5bf4d9ff
Dhurba Dhakal <dhurba.dhakal@ndpc.com>
2026-04-24 12:31:20 +0545

    uloading change

--- resources/views/Frontend/Layout/main.blade.php
+++ resources/views/Frontend/Layout/main.blade.php
@@ -14,8 +14,16 @@
     <link rel="stylesheet" href="{{ asset('frontend/ndpc-responsive.css') }}" />
     <link rel="stylesheet" href="{{ asset('frontend/plugins/OwlCarousel2-2.3.4/owl.carousel.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('frontend/plugins/OwlCarousel2-2.3.4/owl.theme.default.min.css') }}" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('frontend/plugins/lightbox2-2.12.0/dist/css/lightbox.min.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('frontend/plugins/lightbox2-2.12.0/dist/css/lightbox.min.css') }}">
+    {{-- favicon --}}
+    <link rel="icon" type="image/png" href="{{ asset('frontend/favicon/favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('frontend/favicon/favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('frontend/favicon/favicon-96x96.png') }}" sizes="96x96"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('frontend/favicon/apple-touch-icon.png') }}" />
+    <link rel="manifest" href="{{ asset('frontend/favicon/site.webmanifest') }}" />
+    {{-- favicon --}}
 </head>
 
 <body>
@@ -24,10 +32,12 @@
     @yield('content')
 
     @include('frontend.layout.footer')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
     <script src="https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
     <script src="{{ asset('frontend/plugins/OwlCarousel2-2.3.4/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('frontend/plugins/lightbox2-2.12.0/dist/js/lightbox.min.js')}}"></script>
+    <script src="{{ asset('frontend/plugins/lightbox2-2.12.0/dist/js/lightbox.min.js') }}"></script>
     @stack('scripts')
 </body>
 
